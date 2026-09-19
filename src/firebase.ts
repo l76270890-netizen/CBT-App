6fbca38c5ae1a7c8154a174df99779752930d796
lawrence <l76270890@gmail.com>
2026-09-19 17:03:12 +0100

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,12 +4,13 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyDtwn1cNJ-rYT1gePtccg-j_mP_BlWrV-Y",
+  authDomain: "yourcbt-app-d6ee5.firebaseapp.com",
+  projectId: "yourcbt-app-d6ee5",
+  storageBucket: "yourcbt-app-d6ee5.firebasestorage.app",
+  messagingSenderId: "1069735906675",
+  appId: "1:1069735906675:web:0d5538465b8141b86a4906",
+  measurementId: "G-5LMDEVQ59D"
 }
 
 const app = initializeApp(firebaseConfig)
